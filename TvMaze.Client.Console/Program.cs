e08e2826f2c5e9f03e521689d250427fd629be4b
Daan Acohen <daan.acohen@connectingapps.net>
2024-05-08 11:52:26 +0200

    Added console app to try the client package

---
+++ TvMaze.Client.Console/Program.cs
@@ -0,0 +1,9 @@
+﻿using Refit;
+using TvMaze.Client;
+
+Console.WriteLine("Hello, World!");
+
+var api = RestService.For<IMazeApi>("https://api.tvmaze.com");
+var response = await api.GetShowWithDetailsAsync(1, "cast");
+Console.WriteLine($"StatusCode response {response.StatusCode}");
+Console.WriteLine($"{response.Content!.Embedded.Cast.Length} Cast Members");
