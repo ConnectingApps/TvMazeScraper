887aa5aaba935b1475637b1f8c1f9626269a1d53
Daan Acohen <daan.acohen@connectingapps.net>
2024-05-08 16:35:11 +0200

    Fixed build warnings

--- TvMazeScraper.Api/InternalModels/ShowResponse.cs
+++ TvMazeScraper.Api/InternalModels/ShowResponse.cs
@@ -3,6 +3,6 @@
 public class ShowResponse
 {
     public int Id { get; set; }
-    public string Name { get; set; }
-    public Cast[] Cast { get; set; }
+    public string Name { get; set; } = null!;
+    public Cast[] Cast { get; set; } = null!;
 }
